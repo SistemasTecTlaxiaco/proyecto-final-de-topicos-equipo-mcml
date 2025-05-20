a4fc5f1f0cffc7d1394b83ff241edc2bf6d6a595
carla1122 <bautista@letras.com>
2025-05-19 22:18:21 -0600

    Declaracion de la variable id

--- AppAgendaTareas/Usuario.cs
+++ AppAgendaTareas/Usuario.cs
@@ -7,7 +7,10 @@ using System.Threading.Tasks;
 namespace AppAgendaTareas
 {
     internal class Usuario
-    {// Se declara la clase 'Usuario' como 'internal', lo que significa que solo será accesible dentro del mismo ensamblado/proyecto.
+    {// Se declara la clase 'Usuario' como 'internal', lo que significa que solo será accesible dentro del mismo ensamblado/proyecto
+
+        public int id { get; set; }
+        // Propiedad pública 'id' de tipo entero. Representa el identificador único del usuario en la base de datos.
 
     }
-    }
+}
