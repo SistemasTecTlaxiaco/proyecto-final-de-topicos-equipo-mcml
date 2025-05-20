419f68c9a94413eea0ffe1d99330d034e1107574
carla1122 <bautista@letras.com>
2025-05-19 21:28:51 -0600

    Declaracion de Mysql

--- AppAgendaTareas/Conexion.cs
+++ AppAgendaTareas/Conexion.cs
@@ -8,6 +8,12 @@ namespace AppAgendaTareas
 {
     internal class Conexion
     {
-     
+        // Dirección del servidor MySQL
+        protected string server = "localhost";
+        protected string port = "3307";
+        // Nombre de la base de datos a la que se conectará.
+        protected string database = "agendatarea";
+        protected string user = "root";
+        protected string password = "maritza";
     }
 }
