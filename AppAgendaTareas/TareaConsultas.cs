15569b40ded0033cd91343e9dfb1d1268cadc72f
carla1122 <bautista@letras.com>
2025-05-19 21:56:39 -0600

    Declaracion de una lista y se inicia conexion de la BD

--- AppAgendaTareas/TareaConsultas.cs
+++ AppAgendaTareas/TareaConsultas.cs
@@ -9,14 +9,17 @@ namespace AppAgendaTareas
 {
     internal class TareaConsultas
     {
-
+        private ConexionMySql mConexion; // Objeto para manejar la conexión a MySQL.
+        private List<Tarea> mTareas; // Lista para almacenar tareas temporalmente (aunque no se usa en el código mostrado).
 
 
 
         public TareaConsultas()
         {
-
-
+            mConexion = new ConexionMySql(); // Inicializa la conexión a la base de datos.
+            mTareas = new List<Tarea>(); // Inicializa la lista de tareas.
         }
+
+
     }
 }
