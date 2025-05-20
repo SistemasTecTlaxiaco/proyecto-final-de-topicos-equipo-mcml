dca738b5fd5426897ca58e2dc19595b3b2316901
Maritza2893 <GarciaMaritza@coding.com>
2025-05-19 21:57:39 -0600

    Metodo bool agregar tarea

--- AppAgendaTareas/TareaConsultas.cs
+++ AppAgendaTareas/TareaConsultas.cs
@@ -19,7 +19,13 @@ namespace AppAgendaTareas
             mConexion = new ConexionMySql(); // Inicializa la conexión a la base de datos.
             mTareas = new List<Tarea>(); // Inicializa la lista de tareas.
         }
+        // Método para agregar una nueva tarea a la base de datos.
+        public bool agregarTarea(Tarea tarea)
+        {
+            // Cadena SQL para insertar una tarea.
+            string INSERT = "INSERT INTO tarea (titulo, descripcion, fecha_vencimiento, categoria, prioridad) " +
+                "VALUES (@titulo, @descripcion, @fecha, @categoria, @prioridad)";
+        }
 
-
+        }
     }
-}
